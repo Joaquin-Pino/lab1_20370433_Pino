146a165572311c1f14882d572dd8b86bb6008d30
Joaquin <joaquin.pino@usach.cl>
2025-09-10 22:26:25 -0300

    se arregla funcion rf8 y rf06 para seguir con el paradigma funcional

--- TDAbiblioteca.rkt
+++ TDAbiblioteca.rkt
@@ -6,12 +6,30 @@
 
 ; definicion
 (define (crear-biblioteca libros usuarios prestamos max-libros dias-max
-                          tasa-multa limite-deuda dias-retraso fecha-inicial) ;RF05
+                          tasa-multa limite-deuda dias-retraso fecha-inicial ) ;RF05
   (list libros usuarios prestamos max-libros dias-max
-        tasa-multa limite-deuda dias-retraso fecha-inicial))
+        tasa-multa limite-deuda dias-retraso fecha-inicial  ))
 
 ;selectores
-(define (libro-en-biblioteca? biblioteca libro); me quivoque XD, redundatne con lo que hace rf09
+(define (libros-en-biblioteca biblioteca) (obtener-dato biblioteca 0))
+(define (usuarios-biblioteca biblioteca) (obtener-dato biblioteca 1))
+(define (obtener-prestamos biblioteca) (obtener-dato biblioteca 2))
+(define (obtener-max-libros biblioteca) (obtener-dato biblioteca 3))
+(define (obtener-max-dias-prestamo biblioteca) (obtener-dato biblioteca 4))
+(define (obtener-tasa-multa biblioteca) (obtener-dato biblioteca 5))
+(define (obtener-limite-deuda biblioteca) (obtener-dato biblioteca 6))
+(define (obtener-biblioteca-dias-retraso biblioteca) (obtener-dato biblioteca 7))
+(define (get-fecha biblioteca) (obtener-dato biblioteca 8))
+
+(define (get-historial-prestamos biblioteca) (obtener-dato biblioteca 9))
+
+(define (usuario-presente? lista-usr id-usr)
+  (cond
+    ((null? lista-usr) #f)
+    ((= (id-usuario (car lista-usr)) id-usr) #t)
+    (else (usuario-presente? (cdr lista-usr) id-usr))))
+
+(define (libro-en-biblioteca? biblioteca libro)
   (let ((lista-libros (libros-en-biblioteca biblioteca))
         (id-libro-ingresar (get-libro-id libro)))
     (define (buscar-id lista-lib)
@@ -21,43 +39,52 @@
         (else (buscar-id (cdr lista-lib)))))
     (buscar-id lista-libros)))
 
-(define (libros-en-biblioteca biblioteca) (obtener-dato biblioteca 0))
-(define (usuarios-biblioteca biblioteca) (obtener-dato biblioteca 1))
-(define (obtener-prestamos biblioteca) (obtener-dato biblioteca 2))
-(define (obtener-max-libros biblioteca) (obtener-dato biblioteca 3))
-(define (obtener-max-dias-prestamo biblioteca) (obtener-dato biblioteca 4))
-(define (obtener-tasa-multa biblioteca) (obtener-dato biblioteca 5))
-(define (obtener-limite-deuda biblioteca) (obtener-dato biblioteca 6))
-(define (obtener-biblioteca-dias-retraso biblioteca) (obtener-dato biblioteca 7))
-(define (obtener-fecha-actual biblioteca) (obtener-dato biblioteca 8))
-
+;obtener usuario RF08
+;ahora esta funcion cumple con el paradigma funcional
+(define (obtener-usuario biblioteca id)
+  (let ((lst-usrs (usuarios-biblioteca biblioteca)))
+    (define (aux lst)
+      (cond
+        ((null? lst) '())
+        ((= (id-usuario (car lst)) id) (car lst))
+        (else
+         (aux (cdr lst)))))
+    (aux lst-usrs)))
+      
 ;modificadores
+; ya no se rompe con el paradigma funcional
 (define (agregar-libro biblioteca libro) ;RF06
   (if (not (libro-en-biblioteca? biblioteca libro))
       (let ((lista-libros (libros-en-biblioteca biblioteca)))
         (let ((nuevos-libros (agregar-inicio-lista libro lista-libros))); se agrega libro al inicio
-          (modificar-elemento biblioteca 0 nuevos-libros))) ;se entrega la biblioteca modificada
+          (crear-biblioteca nuevos-libros (usuarios-biblioteca biblioteca)
+                            (obtener-prestamos biblioteca)
+                            (obtener-max-libros biblioteca)
+                            (obtener-max-dias-prestamo biblioteca)
+                            (obtener-tasa-multa biblioteca)
+                            (obtener-limite-deuda biblioteca)
+                            (obtener-biblioteca-dias-retraso biblioteca)
+                            (get-fecha biblioteca)
+                            ))) ;se entrega la biblioteca modificada
       biblioteca ;; se entrega la biblioteca sin cambios
       ))
 
 ;registrar usuario RF07
-(define (registrar-usuario biblioteca usuario)
-  (define (usuario-presente? lista-usr id-usr)
-    (cond
-      ((null? lista-usr) #f)
-      ((= (id-usuario (car lista-usr)) id-usr) #t)
-      (else (usuario-presente? (cdr lista-usr) id-usr))
-      )
-    )
-  
-  (let ((id-usr (id-usuario usuario))
+(define (registrar-usuario biblioteca usuario) 
+  (let ((id-usr (id-usuario usuario)) 
         (lista-usuarios (usuarios-biblioteca biblioteca)))
     (if (usuario-presente? lista-usuarios id-usr)
         biblioteca; si el usuario esta presente retornamos biblioteca
-        (modificar-elemento biblioteca 1 (agregar-final-lista usuario lista-usuarios))
-        )
-    )
-  )
+        (let ((lista-con-nuevo-usuario (agregar-final-lista usuario lista-usuarios)))
+          (crear-biblioteca (libros-en-biblioteca biblioteca) lista-con-nuevo-usuario
+                            (obtener-prestamos biblioteca)
+                            (obtener-max-libros biblioteca)
+                            (obtener-max-dias-prestamo biblioteca)
+                            (obtener-tasa-multa biblioteca)
+                            (obtener-limite-deuda biblioteca)
+                            (obtener-biblioteca-dias-retraso biblioteca)
+                            (get-fecha biblioteca)
+                            )))))
 
 ;pertenencia 
 (define (biblioteca? bib)
@@ -81,23 +108,6 @@
       )
     )
   )
-
-;obtener usuario RF08 
-(define (obtener-usuario biblioteca id)
-  (let ((lista-usrs (usuarios-biblioteca biblioteca)))
-    (define (buscar lista)
-      (cond
-        ((null? lista) null)
-        ((= (id-usuario (car lista)) id) (car lista))
-        (else (obtener-usuario (cdr lista) id))
-        )
-      ) 
-    (buscar lista-usrs)
-    ) 
-  )
-
-
-
 
 ;pruebas
 ;---------------------------------------------------------------------------------
